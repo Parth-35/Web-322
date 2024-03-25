8eed0c2721e35043523d70bb5c4e32a1a3f2ed13
Parth <parthpatel35@outlook.com>
2024-03-25 19:08:24 -0400

    Completed website

--- server.js
+++ server.js
@@ -4,49 +4,49 @@ const path = require("path");
 const express = require('express');
 const app = express();
 
-const HTTP_PORT = process.env.PORT || 8080;
+const HTTP_PORT = process.env.PORT || 3030;
 
 app.use(express.static('public'));
-
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/home.html"));
+  res.render("home")
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/about.html"));
+  res.render("about");
 });
 
 app.get("/lego/sets", async (req,res)=>{
 
-  try{
-    if(req.query.theme){
-      let sets = await legoData.getSetsByTheme(req.query.theme);
-      res.send(sets);
-  
-    }else{
-      let sets = await legoData.getAllSets();
-      res.send(sets);
-    }
-  }catch(err){
-    res.status(404).send(err);
-  }
+  let sets = [];
 
+  try{    
+    if(req.query.theme){
+      sets = await legoData.getSetsByTheme(req.query.theme);
+    }else{
+      sets = await legoData.getAllSets();
+    }
+
+    res.render("sets", {sets})
+  }catch(err){
+    res.status(404).render("404", {message: err});
+  }
+  
 });
 
 app.get("/lego/sets/:num", async (req,res)=>{
   try{
     let set = await legoData.getSetByNum(req.params.num);
-    res.send(set);
+    res.render("set", {set})
   }catch(err){
-    res.status(404).send(err);
+    res.status(404).render("404", {message: err});
   }
 });
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+  res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
 });
-
 
 legoData.initialize().then(()=>{
   app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
